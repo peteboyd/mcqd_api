2c4f79eab0f58d84342c60166ba8783f271f323d
Peter Boyd <peteboyd@users.noreply.github.com>
2013-09-09 12:47:42 -0400

    fixed interface, works properly with new_net_finder

--- test.py
+++ test.py
@@ -42,7 +42,6 @@ def corr():
     dist2 = distance.cdist(coord2,coord2)
     p = mcqd.correspondence(atoms1, atoms2)
     q = mcqd.correspondence_edges(p,dist1, dist2, 0.1)
-    print p
     print q
 
 corr()
