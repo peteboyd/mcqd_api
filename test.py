f8eb757e3397755374833c0f8d77c72cbb2cbfb9
pboyd <peter.g.boyd@gmail.com>
2013-09-29 21:18:59 -0400

    fixed some unnecessary variable calls

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 import numpy as np
 from scipy.spatial import distance
 import sys
+import itertools
 sys.path.append("/home/pboyd/codes_in_development/mcqd_api/build/lib.linux-x86_64-2.7")
 import _mcqd as mcqd
 
@@ -28,6 +29,7 @@ def test_clq():
             edge_matrix[ind1][ind2] = 1
             edge_matrix[ind2][ind1] = 1
 
+        print edge_matrix.size
     return mcqd.maxclique(edge_matrix, graph_size)
 #check = ones(99)
 #check = random(11644)
@@ -35,13 +37,26 @@ def test_clq():
 #print check
 def corr():
     atoms1 = ["C", "O", "C", "O"]
-    coord1 = np.random.random((len(atoms1),3))*np.random.randint(3)
-    dist1 = distance.cdist(coord1,coord1)
+    a1 = [1, 2, 3, 4]
+    #coord1 = np.random.random((len(atoms1),3))*np.random.randint(3)
+    #dist1 = distance.cdist(coord1,coord1)
     atoms2 = ["C", "O", "C", "C", "O", "O", "H", "C", "C"]
-    coord2 = np.random.random((len(atoms2),3))*np.random.randint(3)
-    dist2 = distance.cdist(coord2,coord2)
-    p = mcqd.correspondence(atoms1, atoms2)
-    q = mcqd.correspondence_edges(p,dist1, dist2, 0.1)
-    print p
+    a2 = [1, 2, 0, 0, 3, 0, 0, 4, 0]
 
+    adj = [1 if i==j else 0 for (i,j) in itertools.product(a1, a2)]
+    size = len(list(itertools.product(atoms1, atoms2)))
+    q = np.zeros((size, size))
+
+    for ind, i in enumerate(q):
+        for indj, j in enumerate(i):
+            q[ind][indj] = adj[ind]*adj[indj]
+            
+    q = q - np.identity(len(adj))
+    #coord2 = np.random.random((len(atoms2),3))*np.random.randint(3)
+    #dist2 = distance.cdist(coord2,coord2)
+    #p = mcqd.correspondence(atoms1, atoms2)
+    #q = mcqd.correspondence_edges(p,dist1, dist2, 0.1)
+    #print mcqd.maxclique(q, q.size)
+    print mcqd.maxclique(q, len(adj))
 corr()
+
